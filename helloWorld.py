6d7351082562fee6b2fbcefdd350f65ed9cbdaf0
Tomas Vargas <tovare@Tomass-MacBook-Air.local>
2022-08-13 21:08:36 +0200

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,20 @@
+
+name = input("Enter your name: ")
+
+num = 1
+while num <= 5:
+        print("This is Hello World number",num,",",name)
+        num = num + 1
+            
+else:
+        print("you have recieved enough Hello's,",name)
+    
+ 
+    
+     
+
+
+
+
+
+
---
+++ randomUniform.py
@@ -0,0 +1,11 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.uniform(0,9))
+
+
+  
+
+
